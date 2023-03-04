8a5b06572ebd3bca7ed9ad01d82eca14f0fd7ba4
JasonLe <Dung2781993@gmail.com>
2023-03-04 14:35:05 +1100

    Practice JSON exercise

---
+++ 04032023/exercise.js
@@ -0,0 +1,5 @@
+//Exercise 1: Get the total budget of all movies - need to inogre the tbd movie (Easy)
+
+//Exercise 2: Get the list of action movie (category_name) (Middle)
+
+//Exercise 3: Get all the movie that has the budget > 175.000.000 - need to inogre the tbd movie (Easy)
